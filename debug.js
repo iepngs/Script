22d4bcb24a788dc0ff0c29eec9d43ae40fbe5470
iepngs <iepngs@aliyun.com>
2020-05-27 17:52:32 +0800

    调试代码

--- debug.js
+++ debug.js
@@ -11,25 +11,30 @@ function init(){
     console.log("================")
     console.log(headers)
     if(/.*101$/.test($request.url)){
-        body += 101;
+        body = "101";
         return $done({body});
     }
     if(/.*102$/.test($request.url)){
         headers['custom'] = 'custom'
-        body += 102;
+        body = "102";
         return $done({headers});
     }
     if(/.*103$/.test($request.url)){
         headers['custom'] = 'custom'
-        body += 103;
+        body = "103";
         return $done({headers, body});
     }
     if(/.*104$/.test($request.url)){
         headers['custom'] = 'custom'
-        body += 104;
+        body = "104";
         return $done({header:headers, body:body});
     }
     if(/.*105$/.test($request.url)){
+        headers['custom'] = 'custom'
+        body = "105";
+        return $done({headers:headers, body:body});
+    }
+    if(/.*106$/.test($request.url)){
         return $done({});
     }
     return $done();
