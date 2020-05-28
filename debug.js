d61e75bbafbc1c9966663d900229c939c95d21aa
iepngs <iepngs@aliyun.com>
2020-05-28 11:19:08 +0800

    调试代码

--- debug.js
+++ debug.js
@@ -11,33 +11,38 @@ function init(){
     console.log("================")
     console.log(headers)
     if(/.*101$/.test($request.url)){
+        // 只改body
         body = "101";
-        return $done({body});
+        return $done({request: {body: body}});
     }
     if(/.*102$/.test($request.url)){
+        // 只改header
         headers['custom'] = 'custom'
-        body = "102";
-        return $done({headers});
+        return $done({request: {header: headers}});
     }
     if(/.*103$/.test($request.url)){
+        // 同时改body和header
         headers['custom'] = 'custom'
         body = "103";
-        return $done({headers, body});
+        return $done({request: {headers: headers, body: body}});
     }
     if(/.*104$/.test($request.url)){
+        // 同时改body和headers
         headers['custom'] = 'custom'
         body = "104";
-        return $done({header:headers, body:body});
+        return $done({request: {header:headers, body:body}});
     }
     if(/.*105$/.test($request.url)){
         headers['custom'] = 'custom'
         body = "105";
-        return $done({headers:headers, body:body});
+        return $done({response: {header:headers, body:body}});
     }
     if(/.*106$/.test($request.url)){
-        return $done({});
+        headers['custom'] = 'custom'
+        body = "106";
+        return $done({response: {headers:headers, body:body}});
     }
-    return $done();
+    return $done({});
 }
 
 init();
