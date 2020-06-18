af2e9ed5c2a51a76f759f276fbd1b1ad5ab6573a
iepngs <iepngs@aliyun.com>
2020-06-18 16:36:52 +0800

    debug

---
+++ jd/test.js
@@ -0,0 +1,82 @@
+const $hammer = (() => {
+    const isRequest = "undefined" != typeof $request,
+        isSurge = "undefined" != typeof $httpClient,
+        isQuanX = "undefined" != typeof $task;
+
+    const log = (...n) => { for (let i in n) console.log(n[i]) };
+    const alert = (title, body = "", subtitle = "", link = "") => {
+        if (isSurge) return $notification.post(title, subtitle, body, link);
+        if (isQuanX) return $notify(title, subtitle, (link&&!body ? link : body));
+        log('==============📣系统通知📣==============');
+        log("title:", title, "subtitle:", subtitle, "body:", body, "link:", link);
+    };
+    const read = key => {
+        if (isSurge) return $persistentStore.read(key);
+        if (isQuanX) return $prefs.valueForKey(key);
+    },
+        write = (key, val) => {
+            if (isSurge) return $persistentStore.write(key, val);
+            if (isQuanX) return $prefs.setValueForKey(key, val);
+        };
+    const request = (method, params, callback) => {
+        if (typeof params == "string") {
+            params = { url: params };
+        }
+        const options = {
+            url: params.url,
+            body: params.data
+        };
+        method = method.toUpperCase();
+        console.log("isSurge:", [isSurge, isQuanX])
+        if (isSurge) {
+            if (params.header) {
+                options.header = params.header;
+            }
+            console.log(method == "GET")
+            const _runner = method == "GET" ? $httpClient.get : $httpClient.post;
+            return _runner(options, (response, aaa="") => { 
+                console.log("callbackarg:", [response, aaa])
+                callback(response, null) 
+            });
+        }
+        if (isQuanX) {
+            options.method = method;
+            if (params.header) {
+                options.headers = params.header;
+            }
+            if (options.method == "GET" && typeof options == "string") {
+                options = {
+                    url: options
+                };
+            }
+            $task.fetch(options).then(response => {
+                callback(response, null)
+            }, reason => {
+                callback(null, reason.error)
+            });
+        }
+    };
+    const done = (value = {}) => {
+        if (isQuanX) return isRequest ? $done(value) : null;
+        if (isSurge) return isRequest ? $done(value) : $done();
+    };
+    return { isRequest, isSurge, isQuanX, log, alert, read, write, request, done };
+})();
+
+
+
+
+const options = {
+    url: `http://i.orzzhibo.com/live/livelist`,
+    header: {
+        Cookie: cookie,
+        UserAgent: `Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1`,
+    },
+    data: ""
+};
+
+$hammer.request('GET', options, (response, error) => {
+    $hammer.log("functionid:", function_id, "resp:", response, "err:", error)
+})
+
+$hammer.done();
