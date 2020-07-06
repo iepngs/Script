782125e8e4ad1053c8e93d441d2932b687846e49
iepngs <iepngs@aliyun.com>
2020-07-06 12:51:09 +0800

    automatic finish search task relase

--- qktx/index.js
+++ qktx/index.js
@@ -219,7 +219,7 @@ async function qktx_searchTask(limit=20){
     while(index++ < limit){
         let resp = await (()=>{
             return new Promise(resolve=>{
-                const delay = index ? Math.ceil(32000+Math.random()*1000) : 1000;
+                const delay = index == 1 ? 1000 : Math.ceil(32000+Math.random()*1000);
                 setTimeout(() => {
                     $iosrule.post(options, (error, result, response) => {
                         if(error){
